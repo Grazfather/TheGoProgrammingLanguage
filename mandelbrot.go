18fd50d9d92e97745bf435125b52684481e20670
Grazfather <grazfather@gmail.com>
2017-07-24 22:55:55 -0700

    Complete exercise 3.6

--- mandelbrot.go
+++ mandelbrot.go
@@ -21,17 +21,38 @@ import (
 func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 4096, 4096
+		width, height          = 2048, 2048
 	)
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, newton(z))
+			// Super sample: each corner
+			var samples [4]color.Color
+			var i = 0
+			for sy := 0.; sy < 1.; sy += 0.5 {
+				y := (float64(py)+sy)/height*(ymax-ymin) + +ymin
+				for sx := 0.; sx < 1.; sx += 0.5 {
+					x := (float64(px)+sx)/width*(xmax-xmin) + xmin
+					z := complex(x, y)
+					//samples[i] = mandelbrot(z)
+					samples[i] = newton(z)
+					i++
+				}
+			}
+			// Average the samples and set the single pixel
+			var r, g, b uint32
+			for _, c := range samples {
+				_r, _g, _b, _ := c.RGBA()
+				r += uint32(_r / 257)
+				g += uint32(_g / 257)
+				b += uint32(_b / 257)
+			}
+			r /= 4
+			g /= 4
+			b /= 4
+			img.Set(px, py, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
@@ -39,7 +60,7 @@ func main() {
 
 func mandelbrot(z complex128) color.Color {
 	const iterations = 200
-	const contrast = 15
+	const contrast = 25
 
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
