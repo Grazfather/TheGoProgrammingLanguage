6de09ed6cd3fd041778a2a7a887a2ada8d7cf45c
Grazfather <grazfather@gmail.com>
2017-07-31 20:42:34 -0700

    Complete exercise 3.11 (Float support in comma())

--- comma.go
+++ comma.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 // command inserts commands in a non-negative decimal integer string.
@@ -15,24 +16,48 @@ func comma_recursive(s string) string {
 }
 
 func comma(s string) string {
-	size := (len(s)-1)/3 + len(s)
+	if s == "" {
+		return ""
+	}
 	buf := bytes.Buffer{}
-	nums := s[:]
-	buf.Grow(size)
+
+	// Handle optional sign
+	if s[0] == '+' || s[0] == '-' {
+		buf.WriteByte(s[0])
+		s = s[1:]
+	}
 
 	// First write everything before the first comma
-	n := len(s) % 3
-	if n == 0 && len(s) > 0 {
+	dotindex := strings.LastIndexByte(s, '.')
+	var n int
+	var intpart, decimalpart string
+	if dotindex != -1 {
+		intpart = s[:dotindex]
+		decimalpart = s[dotindex+1:]
+	} else {
+		intpart = s[:]
+		decimalpart = ""
+	}
+	n = len(intpart) % 3
+
+	if n == 0 && len(intpart) > 0 {
 		n = 3
 	}
-	buf.WriteString(nums[:n])
-	nums = nums[n:]
+	buf.WriteString(intpart[:n])
+	intpart = intpart[n:]
 
 	// Now loop 3 chars at a time, with a comma preceding
-	for ; len(nums) >= 3; nums = nums[3:] {
+	for ; len(intpart) >= 3; intpart = intpart[3:] {
 		buf.WriteByte(',')
-		buf.WriteString(nums[:3])
+		buf.WriteString(intpart[:3])
 	}
+
+	// If there's a fractional part, write it.
+	if len(decimalpart) > 0 {
+		buf.WriteByte('.')
+		buf.WriteString(decimalpart)
+	}
+
 	return buf.String()
 }
 
@@ -43,4 +68,14 @@ func main() {
 	fmt.Println(comma("123456"))
 	fmt.Println(comma("12345678"))
 	fmt.Println(comma("1234567890123"))
+	// Floats
+	fmt.Println(comma("+"))
+	fmt.Println(comma("."))
+	fmt.Println(comma("1."))
+	fmt.Println(comma(".5"))
+	fmt.Println(comma("12.3"))
+	fmt.Println(comma("123.45"))
+	fmt.Println(comma("+123456"))
+	fmt.Println(comma("-12345678"))
+	fmt.Println(comma("-1234567.890123"))
 }
