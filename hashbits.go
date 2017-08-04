bdb8c782c2ede3a4b493ef5af0616a7612bd9f0e
Grazfather <grazfather@gmail.com>
2017-08-03 20:08:52 -0700

    hashcount: Use popcount to count bits

--- hashbits.go
+++ hashbits.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"os"
+
+	"popcount"
 )
 
-func countBinaryHamminDistance(b1, b2 []byte) (count int, err error) {
+// Count the number of different bits between two equal-length byte slices. Their length must be a multiple of 8.
+func countBinaryHammingDistance(b1, b2 []byte) (count int, err error) {
 	if len(b1) != len(b2) {
 		return 0, errors.New("Cannot compare strings of unequal length")
 	}
 
-	for i, b := range b1 {
-		for bi := uint(0); bi < 8; bi++ {
-			if (b >> bi & 1) != (b2[i] >> bi & 1) {
-				count += 1
-			}
-		}
+	if len(b1)%8 != 0 {
+		return 0, errors.New("Cannot compare strings whose lengths aren't a multiple of 8")
 	}
+
+	for len(b1) > 0 {
+		s1 := binary.LittleEndian.Uint64(b1[:8])
+		s2 := binary.LittleEndian.Uint64(b2[:8])
+		count += popcount.PopCountTable(s1 ^ s2)
+		b1 = b1[8:]
+		b2 = b2[8:]
+	}
+
 	return
 }
 
@@ -33,6 +42,6 @@ func main() {
 	h1 := sha256.Sum256([]byte(s1))
 	h2 := sha256.Sum256([]byte(s2))
 	fmt.Printf("%s: %x\n%s: %x\n", s1, h1, s2, h2)
-	count, _ := countBinaryHamminDistance(h1[:], h2[:])
-	fmt.Printf("Bits different: %d out of %d\n", count, sha256.Size*8)
+	count, _ := countBinaryHammingDistance(h1[:], h2[:])
+	fmt.Printf("Bits different: %d out of %d\n", count, len(h1)*8)
 }
