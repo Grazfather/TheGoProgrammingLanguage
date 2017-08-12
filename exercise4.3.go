46c91dd4fa5f881192e710e948347c9102c8d79a
Grazfather <grazfather@gmail.com>
2017-08-11 18:56:39 -0700

    Complete exercise 4.3 (reverse ARRAY)

---
+++ exercise4.3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func reverse(a *[16]int) {
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func main() {
+	a := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	fmt.Println(a)
+	reverse(&a)
+	fmt.Println(a)
+}
