9f61edaa8413e57d1179ca953c6fe92be915901e
Grazfather <grazfather@gmail.com>
2017-09-20 19:46:55 -0700

    Remove sample version of command (3.10)

--- comma.go
+++ comma.go
@@ -6,15 +6,6 @@ import (
 	"strings"
 )
 
-// command inserts commands in a non-negative decimal integer string.
-func comma_recursive(s string) string {
-	n := len(s)
-	if n <= 3 {
-		return s
-	}
-	return comma(s[:n-3]) + "," + s[n-3:]
-}
-
 func comma(s string) string {
 	if s == "" {
 		return ""
