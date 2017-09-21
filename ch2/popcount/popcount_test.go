0bced3345797d9d8fa635d33250a98f4a04318f1
Grazfather <grazfather@gmail.com>
2017-09-20 19:56:56 -0700

    fixme move popcount

--- ch2/popcount/popcount_test.go
+++ ch2/popcount/popcount_test.go
@@ -5,25 +5,25 @@ import (
 	"testing"
 )
 
-func BenchmarkPopcountTable(b *testing.B) {
+func BenchmarkPopCountTable(b *testing.B) {
 	rand.Seed(int64(5))
 	for i := 0; i < b.N; i++ {
 		PopCountTable(uint64(rand.Int63()))
 	}
 }
-func BenchmarkPopcountLoop(b *testing.B) {
+func BenchmarkPopCountLoop(b *testing.B) {
 	rand.Seed(int64(5))
 	for i := 0; i < b.N; i++ {
 		PopCountLoop(uint64(rand.Int63()))
 	}
 }
-func BenchmarkPopcountShift(b *testing.B) {
+func BenchmarkPopCountShift(b *testing.B) {
 	rand.Seed(int64(5))
 	for i := 0; i < b.N; i++ {
 		PopCountShift(uint64(rand.Int63()))
 	}
 }
-func BenchmarkPopcountClear(b *testing.B) {
+func BenchmarkPopCountClear(b *testing.B) {
 	rand.Seed(int64(5))
 	for i := 0; i < b.N; i++ {
 		PopCountClear(uint64(rand.Int63()))
