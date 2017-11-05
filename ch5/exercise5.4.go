6e0ffc3fbaeaff4ed7ace07bbfd927e5b9acc65b
Grazfather <grazfather@gmail.com>
2017-11-05 09:56:13 -0800

    Complete exercise 5.4 (Dump hrefs)

---
+++ ch5/exercise5.4.go
@@ -0,0 +1,38 @@
+// Exercise 5.4:
+// Extend the visit function so that it extracts other kinds of links from the
+// document, such as images, scripts, and style sheets.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode &&
+		(n.Data == "a" || n.Data == "img" || n.Data == "script" || n.Data == "style") {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
